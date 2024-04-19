d710b8d2cafd4c9ec175465e454c42b94e8e7cbe
Georgi Kerkelov <gkerkelov03@abv.bg>
2024-04-19 12:00:08 +0300

    remove the layout module from the main module

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,24 +1,57 @@
-import { Component, OnDestroy } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { jwtTokenKey } from '../core/constants/local-storage-keys';
 import { sidenavItems } from '../core/constants/sidenav-items';
+import { Theme } from '../core/enums/theme';
 import { SidenavItem } from '../core/models/side-nav-item.model';
+import { CurrentUserService } from '../core/services/current-user.service';
+import { LocalStorageService } from '../core/services/local-storage.service';
+import { ThemeService } from '../core/services/theme.service';
 
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnDestroy {
+export class MainComponent {
     sidenavItems: SidenavItem[] = sidenavItems;
     isOpen: boolean = false;
+    isLightMode: boolean;
+    changeIcon: boolean = true;
 
-    constructor(private dialogRef: MatDialog) {}
-
-    ngOnDestroy(): void {
-        this.dialogRef.closeAll();
+    constructor(
+        private router: Router,
+        private localStorageUtil: LocalStorageService,
+        public currentUserService: CurrentUserService,
+        public themeService: ThemeService
+    ) {
+        this.themeService.initTheme();
+        this.isLightMode = this.themeService.isLightMode();
     }
+    ngOnDestroy(): void {}
 
     onSidebarToggle(): void {
         this.isOpen = !this.isOpen;
+    }
+
+    navigateToProfile(): void {
+        const userId = this.currentUserService.currentUser!.id;
+        this.router.navigate(['main/users', userId]);
+    }
+
+    toggleLightMode(event: Event): void {
+        event.stopPropagation();
+        this.changeIcon = !this.changeIcon;
+        this.isLightMode = this.themeService.isLightMode();
+
+        this.isLightMode
+            ? this.themeService.update(Theme.darkMode)
+            : this.themeService.update(Theme.lightMode);
+    }
+
+    logout(): void {
+        this.localStorageUtil.deleteItem(jwtTokenKey);
+        this.currentUserService.clearCurrentUser();
+        this.router.navigate(['public']);
     }
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioButton } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { SharedModule } from '../shared/shared.module';
-import { LayoutModule } from './layout/layout.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 
@@ -14,8 +15,9 @@ import { MainComponent } from './main.component';
         CommonModule,
         MainRoutingModule,
         SharedModule,
-        LayoutModule,
         MatSidenavModule,
+        MatButtonModule,
+        MatToolbarModule,
         MatRadioButton,
         MatIconModule,
     ],
